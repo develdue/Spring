a483dee3ea7c94b54dce0008f6c599e496951c84
develdue <113408873+develdue@users.noreply.github.com>
2022-11-01 12:46:29 +0900

    싱글톤 무상태성인지 아닌지 예측하기

--- Test7/src/SingletonMain.java
+++ Test7/src/SingletonMain.java
@@ -1,17 +1,57 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Singleton {
 
-   private int price; //접근지정자는 상관 없음 static 도 마찬가지, 이렇게 하면 안되는 이유 -> 
-	
-   private static final Singleton instance = new Singleton();//상수(변하지 않는수), Singleton 타입의 instance라는 변수는 새로운 Singleton객체가 있다.
+	private Repository repository = new Repository();
+	private static final Singleton instance = new Singleton();//상수(변하지 않는수), Singleton 타입의 instance라는 변수는 새로운 Singleton객체가 있다.
    															 //static 으로 한 이유 -> static이란 필드나 변수를 시스템에(jvm) 저장.
+   															 
+	private Singleton() {
+		
+	}
    
-   private Singleton() {//private이므로 이 생성자 호출 불가능. 이렇게 한 이유 -> 
-      
-   }
-   
-   public Singleton getInstance() {//이 클래스에서 유일하게 호출가능. getInstance()를 사용하면 instance를 반환. 
-	   								//틀림 -> 
+   public static Singleton getInstance() {
       return instance;
    }
+   
+   public Item setRepository(int price , String name) {
+	   Item item = new Item();
+	   item.setPrice(price);
+	   item.setName(name);
+	   repository.setItem(item);
+	   
+	   return item;
+   }
+}
+
+class Repository {
+	
+	private static Map<String , Item> store = new HashMap<>();
+	
+	public void setItem(Item item) {
+		store.put(item.getName(), item);
+	}
+	
+	//그냥 알아야함
+	public Item getItem(String name) {
+		return store.get(name);
+	}
+	
+	public void clearStore() {
+		store.clear();
+	}
+}
+
+class Item {
+	
+	private int price;
+	private String name;
+	
+	public void setPrice(int price) {this.price = price; }
+	public void setName(String name) {this.name = name; }
+	
+	public int getPrice() {return price; }
+	public String getName() {return name; }
 }
  
