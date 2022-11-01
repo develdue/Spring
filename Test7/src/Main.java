3526148d2994a60c846c6f88c1a6219ab4767a97
develdue <113408873+develdue@users.noreply.github.com>
2022-11-01 16:01:13 +0900

    싱글톤 연습 및 어떻게 구동되는지 확인

--- Test7/src/Main.java
+++ Test7/src/Main.java
@@ -3,7 +3,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		Singleton single = Singleton.getInstance();
-
+		single.setRepository(500, "서재연");
 		
+		Singleton single2 = Singleton.getInstance();
+		single2.setRepository(400, "tjwodus");
+		System.out.println(single.repository);
+		
+		System.out.println(single.getRepository().getItem("서재연").getPrice());
+		single2.repository= null;
+		single2.repository= new Repository();
+		System.out.println(single2.repository);
+		System.out.println(single2.getRepository().getItem("tjwodus").getPrice());
 	}
+}
+
+class Member {
+	private Long memberId;
+	private String memberName;
+	private String Grade; //enum 사용 VIP / BASIC
 }
--- Test7/src/SingletonMain.java
+++ Test7/src/SingletonMain.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 class Singleton {
 
-	private Repository repository = new Repository();
+	public Repository repository = new Repository(); //수정 못하게 하려고 접근지정자 private, 필드값
 	private static final Singleton instance = new Singleton();//상수(변하지 않는수), Singleton 타입의 instance라는 변수는 새로운 Singleton객체가 있다.
    															 //static 으로 한 이유 -> static이란 필드나 변수를 시스템에(jvm) 저장.
    															 
@@ -11,25 +11,29 @@ class Singleton {
 		
 	}
    
+	
+	public Repository getRepository () {
+		return repository;
+	}
+	
    public static Singleton getInstance() {
       return instance;
    }
    
-   public Item setRepository(int price , String name) {
-	   Item item = new Item();
-	   item.setPrice(price);
+   public Item setRepository(int price , String name) {//setRepository 매개변수 int와 name을 집어 넣는다.
+	   Item item = new Item(); //Item객체 생성
+	   item.setPrice(price); 
 	   item.setName(name);
-	   repository.setItem(item);
+	   repository.setItem(item);//setItem메소드에 item매개변수를 집어넣으면 이름을 저장하고 아이템에 저장된 이름을 가져오고 item 주소값 store에 세팅.
 	   
-	   return item;
+	   return item; //이후 아이템 주소값 반환
    }
 }
-
 class Repository {
 	
 	private static Map<String , Item> store = new HashMap<>();
 	
-	public void setItem(Item item) {
+	public void setItem(Item item) { //Item 타입의 매개변수를 넣으면 store 맵 변수에 item에 저장된 이름과 주소값 세팅
 		store.put(item.getName(), item);
 	}
 	
@@ -42,7 +46,6 @@ class Repository {
 		store.clear();
 	}
 }
-
 class Item {
 	
 	private int price;
